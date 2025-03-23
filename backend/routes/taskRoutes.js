ef8ebd4c44038a3a9c2d596fe4035717a2499507
likanug <likanug.g@qq.com>
2025-03-23 20:09:25 +0800

    添加POST/PUT/ELETE的API路由

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -5,4 +5,16 @@ router.get("/", (_, res) => {
   res.status(200).json({ mesage: "Get All Task" });
 });
 
+router.post("/", (_, res) => {
+  res.status(200).json({ mesage: "Create Task" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} update.` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Task ${req.params.id} delete.` });
+});
+
 module.exports = router;
