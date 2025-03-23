0c1cc8c877eef1cc9fea7f2a34c73c95f753c0d1
likanug <likanug.g@qq.com>
2025-03-23 19:34:33 +0800

    添加一个“/api/tasks”的路由

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
+
+app.get("/api/tasks", (req, res) => {
+  res.send("Get All Tasks");
+});
