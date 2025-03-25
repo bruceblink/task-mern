b66a6d3e7f6ac29c13e42885a43b42280c4abbc9
likanug <likanug.g@qq.com>
2025-03-25 10:58:50 +0800

    添加express-async-handler的支持

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,21 +1,23 @@
-const getTasks = (_, res) => {
-  res.status(200).json({ message: "Get All Tasks" });
-};
+const asyncHandler = require("express-async-handler");
 
-const setTask = (req, res) => {
+const getTasks = asyncHandler(async (_, res) => {
+  res.status(200).json({ message: "Get All Tasks" });
+});
+
+const setTask = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please enter a task");
   }
   res.status(200).json({ mesage: "Create Task" });
-};
+});
 
-const putTask = (req, res) => {
+const putTask = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Task ${req.params.id} update.` });
-};
+});
 
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Task ${req.params.id} delete.` });
-};
+});
 
 module.exports = { getTasks, setTask, putTask, deleteTask };
