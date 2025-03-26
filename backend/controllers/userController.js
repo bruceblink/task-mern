7ea747f217b0e35ca307f97db8808a1587d8c091
likanug <likanug.g@qq.com>
2025-03-26 12:03:16 +0800

    更新userController,添加用户数据写入MongoDB及password加密的代码逻辑

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,32 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register User successful" });
+  // deconstruct request body
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User Exists");
+  }
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  bcrypt.hash(password, salt);
+
+  const user = await User.create({ name, email, password: hashedPassword });
+  if (user) {
+    res.status(201).json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
