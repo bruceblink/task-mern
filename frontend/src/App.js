ee6a3ab144ae7635bd6c1f61e6ca5d9048641ba4
likanug <likanug.g@qq.com>
2025-03-28 14:31:40 +0800

    新增Header组件，在App.js中添加Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Header from "./components/Header";
 function App() {
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className='container'>
+        {/**添加Header */}
+        <Header />
+        {/*在路由中添加Components*/}
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
