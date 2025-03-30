e69af3518f40869843c73b4318bf2f2fcb39fa89
likanug <likanug.g@qq.com>
2025-03-30 11:32:37 +0800

    增加authService以及调用后端API的配置

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const localuser = JSON.parse(localStorage.getItem("user"));
 const initialState = {
@@ -8,6 +9,23 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -20,7 +38,18 @@ export const authSlice = createSlice({
       state.message = "";
     },
   },
-  extraReducers: () => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, actions) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = actions.payload;
+        state.user = null;
+      });
+  },
 });
 
 export const { reset } = authSlice.actions;
