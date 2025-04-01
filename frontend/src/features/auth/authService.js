13d4f825fa0d59f16d455e03a3908ea446cd38d0
likanug <likanug.g@qq.com>
2025-04-01 14:49:53 +0800

    更新API_URL地址，修复login登录失败的问题

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "/api/users";
+const API_URL = "/api/users/";
 const register = async (userData) => {
   //call backend service API
   const response = await axios.post(API_URL, userData);
