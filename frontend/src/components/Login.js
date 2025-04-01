6710124f6d93d0ca68757aaa26054dcfe8189211
likanug <likanug.g@qq.com>
2025-04-01 14:05:22 +0800

    添加Login的细节

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,18 +1,43 @@
 import { FaSignInAlt } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "./Spinner";
+
 const Login = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
   const { email, password } = formData;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) toast.error(message);
+    if (isSuccess || user) navigate("/");
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = { email, password };
+    dispatch(login(userData));
   };
-  return (
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <>
       <section className='heading'>
         <h1>
@@ -54,4 +79,5 @@ const Login = () => {
     </>
   );
 };
+
 export default Login;
